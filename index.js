4ebecde54d485d938863774f3223bb362f651dcc
tetianapiddubna <tpiddubnase@gmail.com>
2024-01-28 18:53:34 +0100

    fix of the initial calculation

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ let weight = prompt("Enter your weight")
 let height = prompt("Enter your height")    
 
 
-let heightMeters = height / 100
-let BMI = weight / height
+let BMI = weight / (height * height)
 let roundDigit = Math.trunc(BMI) 
 
-alert(`Here is your ${roundDigit}`)
+alert(`Here is your BMI calculated ${roundDigit}`)
