3362a54723586f3d70aab3736359f1c31380b04a
tetianapiddubna <tpiddubnase@gmail.com>
2024-01-29 18:38:21 +0100

    new updates

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-let weight = prompt("Enter your weight")
-let height = prompt("Enter your height")    
+// let weight = prompt("Enter your weight")
+// let height = prompt("Enter your height")    
 
 
-let BMI = weight / (height * height)
-let roundDigit = Math.trunc(BMI) 
+// let BMI = weight / (height * height)
+// let roundDigit = Math.trunc(BMI) 
 
-alert(`Here is your BMI calculated ${roundDigit}`)
+// alert(`Here is your BMI calculated ${roundDigit}`)
+let age = prompt("Enter your age")
+let ageInDays = (age - 90)*365
+let ageInWeeks = (age - 90)*52
+let ageInMonth = (age - 90)*12
+
+console.log(`You have left in days ${ageInDays} , in weeks ${ageInWeeks}, in months ${ageInMonth}`);
