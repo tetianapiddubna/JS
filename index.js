fc3a20618d513ea553c56b14ac21375e5b1122ff
tetianapiddubna <tpiddubnase@gmail.com>
2024-01-30 14:25:30 +0100

    adding new solution

--- index.js
+++ index.js
@@ -6,9 +6,18 @@
 // let roundDigit = Math.trunc(BMI) 
 
 // alert(`Here is your BMI calculated ${roundDigit}`)
-let age = prompt("Enter your age")
-let ageInDays = (age - 90)*365
-let ageInWeeks = (age - 90)*52
-let ageInMonth = (age - 90)*12
+// let age = prompt("Enter your age")
+// let ageInDays = (age-90)*365
+// let ageInWeeks = (age-90)*52
+// let ageInMonth = (age-90)*12
 
-console.log(`You have left in days ${ageInDays} , in weeks ${ageInWeeks}, in months ${ageInMonth}`);
+// console.log(`You have left in days ${ageInDays} , in weeks ${ageInWeeks}, in months ${ageInMonth}`);
+let bill = Number(prompt("Welcome to the tip calculator. What was the total bill?"))
+let tip = Number(prompt("How much tip would you like to give? 10, 12, or 15"))
+let people = Number(prompt("How much people to split the bill?"))
+
+let tipPercent = tip / 100
+let totalTip = bill * tipPercent
+let total = totalTip + bill
+let totalPerPerson = total / people
+alert(`Each person should pay ${totalPerPerson}`)
